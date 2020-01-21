9bb2c4f36fbd1d4786dd21774e0cd7ca9d57d7d9
cjohannsen <christoph.johannsen@plentymarkets.com>
2020-01-21 17:49:56 +0100

    CHANGE SetDraftWidget

--- src/Widgets/Item/SetDraftWidget.php
+++ src/Widgets/Item/SetDraftWidget.php
@@ -3,9 +3,10 @@
 namespace Ceres\Widgets\Item;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use Ceres\Widgets\Helper\Factories\Settings\ValueListFactory;
+use Ceres\Widgets\Helper\Factories\WidgetDataFactory;
 use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
 use Ceres\Widgets\Helper\WidgetCategories;
-use Ceres\Widgets\Helper\Factories\WidgetDataFactory;
 use Ceres\Widgets\Helper\WidgetTypes;
 
 class SetDraftWidget extends BaseWidget
@@ -15,8 +16,8 @@ class SetDraftWidget extends BaseWidget
     public function getData()
     {
         return WidgetDataFactory::make("Ceres::SetDraftWidget")
-            ->withLabel("Widget.setDraftLabel")
-            ->withPreviewImageUrl("/images/widgets/plentymarkets-logo.svg")
+            ->withLabel("set draft mockup")
+            ->withPreviewImageUrl("/images/plentymarkets-logo.svg")
             ->withType(WidgetTypes::ITEM)
             ->withCategory(WidgetCategories::ITEM)
             ->withPosition(-1)
@@ -28,10 +29,16 @@ class SetDraftWidget extends BaseWidget
         /** @var WidgetSettingsFactory $settingsFactory */
         $settingsFactory = pluginApp(WidgetSettingsFactory::class);
 
-        // $settingsFactory->createCustomClass();
-        // $settingsFactory->createAppearance();
-        // $settingsFactory->createButtonSize();
-        // $settingsFactory->createSpacing();
+        $settingsFactory->createSelect('draftVersion')
+            ->withDefaultValue(1)
+            ->withName('draft version')
+            ->withListBoxValues(
+                ValueListFactory::make()
+                ->addEntry(1, 'Widget.widgetNum1')
+                ->addEntry(2, 'Widget.widgetNum2')
+                ->addEntry(3, 'Widget.widgetNum3')
+                ->toArray()
+            );
 
         return $settingsFactory->toArray();
     }
